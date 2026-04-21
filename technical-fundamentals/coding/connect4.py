e281e4b4e0e562ebe4aa8a9385ccdf010656e660
Exequiel Silvestre <exesilvestre@gmail.com>
2026-04-20 23:12:06 -0300

    Connect4 with play mode

--- technical-fundamentals/coding/connect4.py
+++ technical-fundamentals/coding/connect4.py
@@ -72,18 +72,19 @@ class Connect4:
         self.empty_cell = empty_cell
         self.board = [[empty_cell for _ in range(width)] for _ in range(height)]
         self.current_player = player_1
-
     
+
     def display(self):
         print(' ', *list(range(self.width)))
         for i, row in enumerate(self.board):
             print(i, *row)
     
+
     def play(self, col_index):
         row_index = 0
         if self.board[row_index][col_index] != self.empty_cell:
-            raise Exception(f'Column {col_index} is full')
-
+            raise Exception('This column is full')
+        
         for row_index in range(self.height):
             if row_index < self.height - 1 and self.board[row_index + 1][col_index] == self.empty_cell:
                 continue
@@ -91,24 +92,39 @@ class Connect4:
 
         self.board[row_index][col_index] = self.current_player
 
-        if self.check_winner(self.current_player, row_index, col_index):
-            print(f"The {self.current_player} won!")
+        if self.check_win(row_index, col_index, self.current_player):
+            print(f"The player {self.current_player} won!")
             return self.current_player
-
+        
         self.current_player = (self.player_1 if self.current_player == self.player_2 else self.player_2)
     
-
-    def check_winner(self, player, row_index, col_index):
+    
+    def check_win(self, row_index, col_index, current_player):
         row = self.board[row_index]
         col = [self.board[i][col_index] for i in range(self.height)]
-        diag_inc = self.make_diag_inc(row_index, col_index)
+        diag_inc = self.make_diag_inc(row_index, col_index) 
         diag_dec = self.make_diag_dec(row_index, col_index)
 
-        lines = row, col, diag_dec, diag_inc
+        lines = row, col, diag_inc, diag_dec
 
-        return any(player * 4 in ''.join(line) for line in lines )
+        return any(current_player * 4 in ''.join(line) for line in lines)
+    
 
 
+    def make_diag_inc(self, row_index, col_index):
+        diag_inc = []
+        r, c = row_index, col_index
+        while r < self.height - 1 and c > 0:
+            r += 1
+            c -= 1
+
+        while r > 0 and c < self.width:
+            diag_inc.append(self.board[r][c])
+            r -= 1
+            c += 1
+
+        return diag_inc
+    
     def make_diag_dec(self, row_index, col_index):
         diag_dec = []
         r, c = row_index, col_index
@@ -120,28 +136,30 @@ class Connect4:
             diag_dec.append(self.board[r][c])
             r += 1
             c += 1
+        
         return diag_dec
     
-    def make_diag_inc(self, row_index, col_index):
-        diag_inc = []
-        r, c = row_index, col_index
-        while r < self.height - 1 and c > 0:
-            r += 1
-            c -= 1
-        
-        while r > 0 and c < self.width:
-            diag_inc.append(self.board[r][c])
-            r -= 1
-            c += 1
-        
-        return diag_inc
-
-
-c = Connect4()
-c.display()
-
 
 t = TestConnect4()
 t.setup_method()
+t.test_row()
 
-t.test_col()
+def play_console():
+    player_name_1 = str(input("Type the name for the first player: "))
+    player_name_2 =  str(input("Type the name for the second player: "))
+    game = Connect4(player_1=player_name_1, player_2=player_name_2)
+    try:
+        game.display()
+        while True:
+            col = int(input(f"The player {game.current_player} has to select a column: "))
+            play = game.play(col)
+            game.display()
+
+            if play:
+                print("Match finished!")
+                return
+    except:
+        raise Exception(f"Error")   
+
+play_console() 
+    
